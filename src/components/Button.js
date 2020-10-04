bdaa3112c3979d18ad34862b7d9561d733c94acd
Ivailo663 <ivailo663@gmail.com>
2020-10-04 19:35:00 +0300

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,10 @@
 import React from "react";
 
 function Button(props) {
-  // const func = () => {
-  //   console.log(props.isRegValid);
-  //   if (props.sequnce || props.isRegValid) {
-  //     props.checkSequence();
-  //   } else {
-  //     return "";
-  //   }
-  // };
   return (
     <button
       className={`check-btn ${props.isRegValid ? "" : "not-allowed"}`}
       onClick={props.sequnce || props.isRegValid ? props.checkSequence : ""}
-      // onClick={func}
     >
       Check
     </button>
